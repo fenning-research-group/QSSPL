1513b223327dd3adb6175ad3a2c425a389deab47
Henry <hmvossler@gmail.com>
2023-11-07 11:35:41 -0800

    updates

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name="qsspl",
+    packages=['qsspl'],
+    version="1.0",
+    license="",
+    description="Control Code for the QSSPL setup in the FRG",
+    author="Henry Vossler",
+    author_email="hmvossler@gmail.com",
+    url="https://github.com/fenning-research-group/QSSPL",
+    download_url="",
+    keywords=[
+        "Photoluminescence",
+    ],
+    install_requires=[
+    ]
+)
